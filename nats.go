8eafc0059a02961958e9ba16343067846fb854bf
serchemach <serchemach@gmail.com>
2024-09-13 23:19:16 +0300

    Add the basic connection stuff for nats-streaming

--- nats.go
+++ nats.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/nats-io/stan.go"
 	"os"
+	"time"
 )
 
 func getEnv(key, fallback string) string {
@@ -10,4 +11,22 @@ func getEnv(key, fallback string) string {
 		return value
 	}
 	return fallback
+}
+
+func createConn() (stan.Conn, error) {
+	clusterId := getEnv("STAN_CLUSTER_ID", "test-cluster")
+	clientId := getEnv("STAN_CLIENT_ID", "wb-tech-level-0")
+	stanURL := getEnv("STAN_URL", "nats://nats-streaming:4222")
+
+	return stan.Connect(clusterId, clientId, stan.NatsURL(stanURL))
+}
+
+func messageHandler(m *stan.Msg) {
+
+}
+
+func createSubscription(sc stan.Conn) (stan.Subscription, error) {
+	channelName := getEnv("STAN_CHANNEL_NAME", "wb-channel")
+
+	return sc.Subscribe(channelName, messageHandler, stan.StartWithLastReceived(), stan.AckWait(20*time.Second))
 }
