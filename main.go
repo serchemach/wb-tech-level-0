6cf56cec4550df9db90342ad8403fb256384e644
serchemach <serchemach@gmail.com>
2024-09-15 15:17:07 +0300

    Refactor main

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bytes"
+	// "bytes"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -9,39 +9,14 @@ import (
 	// "html"
 	"log"
 	"net/http"
-	"time"
+	// "time"
 
 	"github.com/dgraph-io/ristretto"
-	"github.com/jackc/pgx/v5/pgxpool"
+	// "github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
-	"github.com/nats-io/stan.go"
+	// "github.com/nats-io/stan.go"
 	"github.com/serchemach/wb-tech-level-0/nats_stuff"
 )
-
-func messageHandler(m *stan.Msg, dbConn *pgxpool.Pool, cache *ristretto.Cache) {
-	fmt.Printf("Recieved a message: %s\n", m.Data)
-	var order nats_stuff.Order
-	decoder := json.NewDecoder(bytes.NewReader(m.Data))
-	decoder.DisallowUnknownFields()
-	err := decoder.Decode(&order)
-	if err != nil {
-		fmt.Printf("Error while converting the data recieved from the channel: %s\n", err)
-		return
-	}
-
-	fmt.Printf("Recieved an order: %v\n", order)
-	err = insertOrderWithCache(&order, cache, dbConn)
-	if err != nil {
-		fmt.Printf("Error while inserting the order into the database: %s\n", err)
-		return
-	}
-}
-
-func CreateSubscription(sc stan.Conn, dbConn *pgxpool.Pool, cache *ristretto.Cache) (stan.Subscription, error) {
-	channelName := getEnv("STAN_CHANNEL_NAME", "wb-channel")
-
-	return sc.Subscribe(channelName, func(m *stan.Msg) { messageHandler(m, dbConn, cache) }, stan.StartWithLastReceived(), stan.AckWait(20*time.Second))
-}
 
 func main() {
 	err := godotenv.Load()
